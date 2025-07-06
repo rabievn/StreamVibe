1f78a357a29a5f4f7609d37ca12aedf0f13173cc
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2025-07-06 22:00:16 +0500

    refactor: use .modules for scss

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import './Login.scss'
+import styles from './Login.module.scss'
 import { StreamVibeIcon } from '../../assets/icons'
 
 export const Login = () => {
   const { t } = useTranslation()
 
   return (
-    <div className="login">
-      <div className="login-container">
-        <div className="login-header">
+    <div className={styles.login}>
+      <div className={styles.loginContainer}>
+        <div className={styles.loginHeader}>
           <StreamVibeIcon />
           <div>123123213</div>
         </div>
-        <div className="login-form">
-          <div className="login-form__header">{t('login')}</div>
+        <div className={styles.loginForm}>
+          <div className={styles.loginFormHeader}>
+            <div className={styles.loginFormHeaderTitle}>{t('login')}</div>
+          </div>
         </div>
       </div>
     </div>
---
+++ frontend/src/shared/ui/Input/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Input.module.scss'
+
+export const Input = () => {
+  return (
+    <div>
+      Input
+    </div>
+  )
+}
+
