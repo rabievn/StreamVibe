6862f6a99edb7a25bf1a6d3c0eb1bd345bdc1fa4
Nurik <nurmuhammad.rn@gmail.com>
2025-07-11 13:45:05 +0500

    feat: incompletely input & login

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next'
 import styles from './Login.module.scss'
 import { StreamVibeIcon } from '../../assets/icons'
 import { Input } from '../../shared/ui/Input/Input'
+import { Button } from '../../shared/ui/Button/Button'
 
 export const Login = () => {
   const { t } = useTranslation()
@@ -17,9 +18,16 @@ export const Login = () => {
           <div className={styles.loginFormHeader}>
             <div className={styles.loginFormHeaderTitle}>{t('login')}</div>
           </div>
-          <div className={styles.loginFormInputsBlock}>
+          <div className={styles.loginFormInputs}>
             <Input type="text" placeholder={t('email')} />
             <Input type="password" placeholder={t('password')} />
+          </div>
+          <div className={styles.loginFormEnter}>
+            <Button onClick={() => {
+            }} color="red">{t('login')}</Button>
+            <span>{t('or')}</span>
+            <Button onClick={() => {
+            }} color="gray">{t('signup')}</Button>
           </div>
         </div>
       </div>
