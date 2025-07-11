cec3b669488f2eaedf0d1d05340f7cdd028fed9d
Nurik <nurmuhammad.rn@gmail.com>
2025-07-11 18:11:46 +0500

    feat: incompl bg for auth

--- frontend/src/layouts/AuthLayout/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout/AuthLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { StreamVibeIcon } from '../../assets/icons'
 import { SchemeButton } from '../../shared/ui/SchemeButton/SchemeButton'
 import styles from './AuthLayout.module.scss'
+import moviesCards from '../../assets/images/SubContainer.png'
 
 type LoginLayoutProps = {
   children: React.ReactNode;
@@ -19,6 +20,10 @@ export const AuthLayout: React.FC<LoginLayoutProps> = ({ children }) => {
           </div>
         </div>
         {children}
+      </div>
+      <div className={styles.authBackground}>
+        <img src={moviesCards} alt="Movies cards" />
+        <img src={moviesCards} alt="Movies cards" />
       </div>
     </div>
   )
