c3567873da5cdd705361d8794751513f0da1808c
Nurik <nurmuhammad.rn@gmail.com>
2025-07-26 14:06:12 +0500

    feat: connect backend with my db

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import { config } from './config/index.js'
+import app from './app.js'
+import mongoose from 'mongoose'
+
+dotenv.config()
+
+mongoose.connect(config.dbUri)
+  .then(() => {
+    app.listen(config.port, () => {
+      console.log(`Server started on port ${config.port}`)
+    })
+  })
+  .catch(err => {
+    console.error('DB connection error:', err)
+    process.exit(1)
+  })
