2ef18db9c4723107cf27302bbb6cd09ae598b970
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2025-07-26 21:05:52 +0500

    feat: check auth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { UserController } from './controllers/index.js'
+import { checkAuth } from './middleware/UserValidations.js'
 
 const app = express()
 
@@ -9,5 +11,9 @@ app.use(express.json())
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hello from the backend!' })
 })
+
+app.get('/auth/me', checkAuth, UserController.getMe)
+app.get('/auth/signIn', checkAuth, UserController.getMe)
+
 
 export default app
--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express'
-import { getUserById } from '../services/user.service.js'
+import { getUserById } from '../services/UserService.js'
 
-export const getMe = (req: Request, res: Response) => {
+export const signUp = () => {
+
+}
+
+export const signIn = () => {
+
+}
+
+export const getMe = async (req: Request & { userId?: string }, res: Response) => {
   try {
-    const user = getUserById('')
+    const userId = req.userId
+
+    if (!userId) {
+      return res.status(401).json({ message: 'Нет доступа' })
+    }
+
+    const user = await getUserById(userId)
 
     if (!user) {
       return res.status(404).json({ message: 'Пользователь не найден' })
@@ -11,11 +25,7 @@ export const getMe = (req: Request, res: Response) => {
 
     res.json(user)
   } catch (err) {
-    if (err instanceof Error) {
-      console.error(err)
-      res.status(500).json({ message: err.message })
-    } else {
-      res.status(500).json({ message: 'Unexpected error' })
-    }
+    console.error(err)
+    res.status(500).json({ message: err instanceof Error ? err.message : 'Unexpected error' })
   }
 }
