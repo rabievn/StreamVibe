871db926714f8c85af0e1c8a18b72ebdedda0430
Nurmuhammad02 <nurmuhammad.rn@gmail.com>
2025-07-29 21:00:38 +0500

    feat: generate token to user service

--- backend/src/services/token-service.ts
+++ backend/src/services/token-service.ts
@@ -1,26 +1,33 @@
 import UserModel from '../models/user-model.js'
-import jwt from "jsonwebtoken";
-import {config} from "../config/index.js";
-import tokenModel from "../models/token-model.js";
+import jwt from 'jsonwebtoken'
+import { config } from '../config/index.js'
+import tokenModel from '../models/token-model.js'
 
 export class TokenService {
-    static generateTokens(payload: any) {
-        if (!config.jwtSecret) {
-            throw new Error("JWT_SECRET is not defined");
-        }
-        if (!config.jwtRefreshSecret) {
-            throw new Error("JWT_REFRESH_SECRET is not defined");
-        }
-
-        const accessToken = jwt.sign(payload, config.jwtSecret, {expiresIn: '30m'});
-        const refreshToken = jwt.sign(payload, config.jwtRefreshSecret, {expiresIn: '30d'});
-
-        return {accessToken, refreshToken};
+  static generateTokens(payload: any) {
+    if (!config.jwtSecret) {
+      throw new Error('JWT_SECRET is not defined')
+    }
+    if (!config.jwtRefreshSecret) {
+      throw new Error('JWT_REFRESH_SECRET is not defined')
     }
 
-    async saveToken(userId: string, refreshToken: string) {
-        const tokenData = await tokenModel.findOne({user: userId})
+    const accessToken = jwt.sign(payload, config.jwtSecret, { expiresIn: '30m' })
+    const refreshToken = jwt.sign(payload, config.jwtRefreshSecret, { expiresIn: '30d' })
 
+    return { accessToken, refreshToken }
+  }
+
+  static async saveToken(userId: any, refreshToken: string) {
+    const tokenData = await tokenModel.findOne({ user: userId })
+
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken
+      return tokenData.save()
     }
+    const token = await tokenModel.create({ user: userId, refreshToken })
+
+    return token
+  }
 }
 
--- backend/src/services/user-service.ts
+++ backend/src/services/user-service.ts
@@ -2,6 +2,7 @@ import UserModel from '../models/user-model.js'
 import bcrypt from 'bcrypt'
 import * as uuid from 'uuid'
 import {MailService} from "./mail-service.js";
+import { TokenService } from './token-service.js'
 
 export class UserService {
     static async signUp(email: string, password: string) {
@@ -15,6 +16,8 @@ export class UserService {
         const user = await UserModel.create({email, password: hashPassword, activationLink})
 
         await MailService.sendActivationMail(email, activationLink)
+
+        const token = TokenService.generateTokens(user)
     }
 
     static getUserById = async (userId: string) => {
