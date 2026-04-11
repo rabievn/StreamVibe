089e475b45076b8c86c8fdc7f001d2186206bfad
Nurik <nurmuhammad.rn@gmail.com>
2026-04-11 12:18:22 +0500

    feat(frontend): enhance theme switcher with active state indication and improve icon styling

--- frontend/src/shared/ui/LanguageButton/LanguageSwitcher.tsx
+++ frontend/src/shared/ui/LanguageButton/LanguageSwitcher.tsx
@@ -1,7 +1,9 @@
 import i18next from 'i18next'
 import { useDispatch, useSelector } from 'react-redux'
+
 import { selectLanguage } from '../../model/languageSelectors'
 import { setLanguage } from '../../model/languageSlice'
+
 import styles from './LanguageSwitcher.module.scss'
 
 export const LanguageSwitcher = () => {
@@ -20,7 +22,6 @@ export const LanguageSwitcher = () => {
       <button className={`${currentLang === 'ru' ? styles.active : ''}`} onClick={() => handleChangeLanguage('ru')}>RU
       </button>
     </div>
-
   )
 }
 
--- frontend/src/shared/ui/SchemeButton/SchemeSwitcher.tsx
+++ frontend/src/shared/ui/SchemeButton/SchemeSwitcher.tsx
@@ -5,13 +5,23 @@ import {MoonIcon, SunIcon} from '../../../assets/icons'
 import styles from './SchemeSwitcher.module.scss'
 
 export const SchemeSwitcher = () => {
-    const {setMode} = useTheme()
+    const {setMode, mode} = useTheme()
 
     return (
-        <div className={`${styles.schemeButton}`}>
-            <button onClick={() => setMode('light')}><SunIcon/></button>
-            <button onClick={() => setMode('dark')}><MoonIcon/></button>
+        <div className={styles.schemeButton}>
+            <button
+                className={`${mode === 'light' ? styles.active : ''}`}
+                onClick={() => setMode('light')}
+            >
+                <SunIcon/>
+            </button>
+
+            <button
+                className={`${mode === 'dark' ? styles.active : ''}`}
+                onClick={() => setMode('dark')}
+            >
+                <MoonIcon/>
+            </button>
         </div>
     )
 }
-
